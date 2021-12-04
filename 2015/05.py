dc2024171eca0b8f202c8f37f38484202c1430d6
xerorio <lucakchan@gmail.com>
2021-12-04 16:56:53 +0000

    keep trying regex stuff for day 5

--- 2015/05.py
+++ 2015/05.py
@@ -1,4 +1,7 @@
 # part 1
+from string import ascii_lowercase
+
+
 def part_1():
     from string import ascii_lowercase
     alphabet = list(ascii_lowercase)
@@ -27,21 +30,29 @@ def part_1():
 
 # part 2
 def part_2():
-    from string import ascii_lowercase
-    alphabet = list(ascii_lowercase)
+    import re
+    # from string import ascii_lowercase
+    # alphabet = ascii_lowercase
 
     strings = []
-
     with open('05.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
             strings.append(line.strip())
+    
+    # string contains two characters next to each other that are the same
+    regex_string_1 = r'.*(([a-z])\\1).*'
+    pattern = re.compile(regex_string_1)
+    # and that it doesn't contain three in a row
+    regex_string_2 = r'.*[a-z]\\1\\1.*'
+    
+    # string contains at least one letter which repeats with exactly one letter between them
+    regex_string_3 = r'.*[a-z].{1}[a-z].*'
 
     counter = 0
-
-    for i in range(len(strings)):
-        counter += 1
     
-    print(counter)
+    for i in range(len(strings)):
+        matches = pattern.finditer(strings[i])
+        print(list(matches))
 
 part_2()
