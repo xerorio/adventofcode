ee5ac4b5060b88c66760b0d36a1488349edb704b
xerorio <lucakchan@gmail.com>
2021-12-04 09:31:22 +0000

    day 5 part 1

--- 2015/05.py
+++ 2015/05.py
@@ -1,9 +1,8 @@
-# puzzle input: ckczppom
-import hashlib
-puzzle_input = 'ckczppom'
-
 # part 1
 def part_1():
+    from string import ascii_lowercase
+    alphabet = list(ascii_lowercase)
+
     strings = []
 
     with open('05.txt', 'r') as f:
@@ -12,21 +11,37 @@ def part_1():
             strings.append(line.strip())
 
     counter = 0
-    print(strings)
 
     for i in range(len(strings)):
         if (strings[i].count('a') + strings[i].count('e') + strings[i].count('o') + strings[i].count('i') + strings[i].count('u')) >= 3:
             if 'ab' not in strings[i] and 'cd' not in strings[i] and 'pq' not in strings[i] and 'xy' not in strings[i]:
-                if strings[i] == strings[i-1]:
-                    counter += 1
+                for letter in alphabet:
+                    if f'{letter}{letter}' in strings[i]:
+                        counter += 1
+                        break
     
     print(counter)
 
-part_1()
+# part_1()
 
 
 # part 2
 def part_2():
-    pass
+    from string import ascii_lowercase
+    alphabet = list(ascii_lowercase)
 
-# part_2()
+    strings = []
+
+    with open('05.txt', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            strings.append(line.strip())
+
+    counter = 0
+
+    for i in range(len(strings)):
+        counter += 1
+    
+    print(counter)
+
+part_2()
