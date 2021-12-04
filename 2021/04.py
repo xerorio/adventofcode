6c331ba0bf52c708b022b69ea5160ac752e107b5
xerorio <lucakchan@gmail.com>
2021-12-04 16:24:02 +0000

    day 4 and make day 5 file

--- 2021/04.py
+++ 2021/04.py
@@ -1,61 +1,118 @@
 # part 1
 def part_1():
+	from copy import deepcopy
+
+	og = []
 	bingo_numbers = []
-	boards = []
 
 	with open('041.txt', 'r') as f:
-		bingo_numbers = f.read().split(',')
+		for i in f.read().split(','):
+			bingo_numbers.append(int(i))
 
 	with open('042.txt', 'r') as f:
-		orginal_boards = f.read().split('\n\n')
-		for b in orginal_boards:
-			boards.append(list(row.split(' ') for row in b.split('\n')))
+		og = [b.split('\n') for b in f.read().split('\n\n')]
+		for i in range(len(og)):
+			for j in range(len(og[i])):
+				og[i][j] = list(map(int, og[i][j].split()))
+	
+	boards = deepcopy(og)
 
-	# remove empty strings
-	for b in boards:
-		for r in b:
-			while '' in r:
-				r.remove('')
+	def win(board):
+		if ('', '', '', '', '') in board or ('', '', '', '', '') in list(zip(*board)):
+			return True
+		return False
+	
+	winner = []
+	latest_num = 0
 
-	# keep a record of original board
-	boards_og = boards.copy()
+	num_iterator = iter(bingo_numbers)
 
-	# get columns from boards
-	for board in boards:
-		cols = []
-		for i in range(5):
-			c = []
-			for r in board:
-				c.append(r[i])
-			cols.append(c)
-		print(cols)
+	playing = True
 
-	def solve_board(b: list) -> int:
-		counter = 0
-		for n in bingo_numbers:
-			for row in b:
-				if n in row:
-					row.remove(n)
-					print(row)
-					counter += 1
-				if row.count('') == 5:
-					return counter
-			for board in boards:
-				cols = []
-				for i in range(5):
-					c = []
-					for r in board:
-						c.append(r[i])
-					cols.append(c)
+	while playing:
+		latest_num = next(num_iterator)
+		# boards
+		for i in range(len(boards)):
+			# then go through each row
+			for j in range(len(boards[i])):
+				# then each number in a row
+				for k in range(len(boards[i][j])):
+					if boards[i][j][k] == latest_num:
+						boards[i][j][k] = ''
+			if win(boards[i]):
+				winner = boards[i]
+				playing = False
+				break
+	
+	unmarked_sum = 0
+	for r in winner:
+		for n in r:
+			if n != '':
+				unmarked_sum += n
+	
+	print(unmarked_sum * latest_num)
 
-	# for b in boards:
-	# 	print(f'{b}\n{solve_board(b)}')
-
-part_1()
+# part_1()
 
 
 # part 2
 def part_2():
-	pass
+	from copy import deepcopy
+
+	og = []
+	bingo_numbers = []
+
+	with open('041.txt', 'r') as f:
+		for i in f.read().split(','):
+			bingo_numbers.append(int(i))
+
+	with open('042.txt', 'r') as f:
+		og = [b.split('\n') for b in f.read().split('\n\n')]
+		for i in range(len(og)):
+			for j in range(len(og[i])):
+				og[i][j] = list(map(int, og[i][j].split()))
+	
+	boards = deepcopy(og)
+
+	def win(board):
+		if ('', '', '', '', '') in board or ('', '', '', '', '') in list(zip(*board)):
+			return True
+		return False
+	
+	loser = []
+	latest_num = 0
+
+	num_iterator = iter(bingo_numbers)
+
+	playing = True
+
+	while playing:
+		try:
+			number = next(num_iterator)
+		except:
+			break
+		# boards
+		for i in range(len(boards)):
+			# then go through each row
+			for j in range(len(boards[i])):
+				# then each number in a row
+				for k in range(len(boards[i][j])):
+					if boards[i][j][k] == number:
+						boards[i][j][k] = ''
+		for i in range(len(boards)):
+			if not win(boards[i]):
+				playing = True
+				loser = boards[i]
+				break
+			else:
+				playing = False
+	
+	unmarked_sum = 0
+	for r in loser:
+		for n in r:
+			if n != '':
+				unmarked_sum += n
+	print(loser)
+	print(unmarked_sum * latest_num)
 
 # part_2()
